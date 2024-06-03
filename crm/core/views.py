b8a4386bcbf9479c6749fdeebedafa086de309cd
isaque <isaquefernando10@gmail.com>
2024-06-03 09:29:44 -0300

    adicionando navbar e bootstrap

--- crm/core/views.py
+++ crm/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
